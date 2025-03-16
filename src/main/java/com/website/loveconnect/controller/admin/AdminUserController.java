a92b1ecf8ce78c6e1d7377a754e858014d7defb4
dinhductai <dinhductai2501@gmail.com>
2025-03-16 23:16:53 +0700

    chore: update class dto

--- src/main/java/com/website/loveconnect/controller/admin/AdminUserController.java
+++ src/main/java/com/website/loveconnect/controller/admin/AdminUserController.java
@@ -5,14 +5,11 @@ import com.website.loveconnect.dto.response.UserResponse;
 import com.website.loveconnect.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -24,7 +21,7 @@ public class AdminUserController {
 
     @GetMapping(value = "/users")
     public ResponseEntity<ApiResponse<Page<UserResponse>>> getAllUser(@RequestParam(defaultValue = "0") int page,
-                                                                     @RequestParam(defaultValue = "10") int size) {
+                                                                      @RequestParam(defaultValue = "10") int size) {
         return  ResponseEntity.ok(new ApiResponse<>(true,"get list user successful",userService.getAllUser(page,size)));
     }
 }
