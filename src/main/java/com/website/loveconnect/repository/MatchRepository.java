90462c348c878d4d5096a1b3ddc2a9ff21b1625f
WhyNotYud <mtd.contactwithme@gmail.com>
2025-03-24 01:16:19 +0700

    feat: add Match Repository

---
+++ src/main/java/com/website/loveconnect/repository/MatchRepository.java
@@ -0,0 +1,14 @@
+package com.website.loveconnect.repository;
+
+import com.website.loveconnect.entity.Match;
+import com.website.loveconnect.enumpackage.MatchStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Integer> {
+    List<Match> findBySenderIdOrReceiverId(Integer senderId, Integer receiverId);
+    List<Match> findByStatus(MatchStatus status);
+}
