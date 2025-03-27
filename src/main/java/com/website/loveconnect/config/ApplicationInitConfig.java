97a551c6aeef105050429388256004100a047a59
dinhductai <dinhductai2501@gmail.com>
2025-03-27 15:41:58 +0700

    chore: update some comment

--- src/main/java/com/website/loveconnect/config/ApplicationInitConfig.java
+++ src/main/java/com/website/loveconnect/config/ApplicationInitConfig.java
@@ -43,8 +43,6 @@ public class ApplicationInitConfig {
         return args -> {
             //check trong database chưa có bất kì tài khoản admin nào
             if(userRepository.existsByRoleAdminAndStatusActive() == 0) {
-//                var roles = new HashSet<String>();
-//                roles.add(RoleName.ADMIN.name());
                 User user = User.builder()
                         .email("admin@admin.com")
                         .password(passwordEncoder.encode("admin"))
