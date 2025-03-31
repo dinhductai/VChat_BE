9692275db12a8d6d7a48f9b0332c8c900f286eef
dinhductai <dinhductai2501@gmail.com>
2025-03-31 12:11:46 +0700

    chore: update security config endpoint

--- src/main/java/com/website/loveconnect/config/SecurityConfig.java
+++ src/main/java/com/website/loveconnect/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.website.loveconnect.enumpackage.RoleName;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Role;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
@@ -15,6 +17,7 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.util.ArrayList;
@@ -27,23 +30,63 @@ public class SecurityConfig {
     @Value(value = "${jwt.secret}")
     private String secretKey;
 
+    private static final String ADMIN_API_PREFIX = "/api/admin";
+    private static final String USER_API_PREFIX = "";
+    private static final String AUTH_API_PREFIX = "/api/auth";
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
-                .requestMatchers(HttpMethod.POST,"/api/auth/log-in","/api/admin/users/create")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET,"/api/admin/users/{userId}")
-                .hasRole(RoleName.ADMIN.name())
-                .anyRequest().authenticated());
-
+        httpSecurity.authorizeHttpRequests(registry -> configureAuthorization(registry))
         //bản thân là resource server nên dùng
-        httpSecurity.oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> httpSecurityOAuth2ResourceServerConfigurer
+        .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer -> httpSecurityOAuth2ResourceServerConfigurer
                 .jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder())
                         .jwtAuthenticationConverter(jwtConverter())));
 
         //tắt csrf
         httpSecurity.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
         return httpSecurity.build();
+    }
+
+    //hàm config các endpoint
+    private void configureAuthorization(AuthorizeHttpRequestsConfigurer<HttpSecurity>
+                                                .AuthorizationManagerRequestMatcherRegistry registry){
+        //endpoint công khai cho tất cả role và chưa đăng nhập
+        String[] publicPostEndpoint = {
+                AUTH_API_PREFIX+"/log-in",
+                ADMIN_API_PREFIX+"/users/create"
+        };
+
+        //endpoint mà admin và user dùng chung
+        String[] generalPostEndpoint = {
+                ADMIN_API_PREFIX+"/users/profile-image/create"
+        };
+
+        String[] adminGetEndpoint = {
+                ADMIN_API_PREFIX+"/users",
+                ADMIN_API_PREFIX+"/users/{userId}",
+                ADMIN_API_PREFIX,"/users/{userId}/update",
+                ADMIN_API_PREFIX,"/users/search"
+        };
+
+        String[] adminPostEndpoint = {};
+        String[] adminPutEndpoint = {
+                ADMIN_API_PREFIX+"/users/{userId}/block",
+                ADMIN_API_PREFIX+"/users/{userId}/unblock",
+                ADMIN_API_PREFIX,"/users/{userId}/update"
+        };
+        String[] adminDeleteEndpoint = {
+                ADMIN_API_PREFIX+"/users/{userId}/delete"
+        };
+
+        registry.requestMatchers(HttpMethod.POST, publicPostEndpoint).permitAll();
+        registry.requestMatchers(HttpMethod.POST, generalPostEndpoint)
+                .hasAnyRole(RoleName.ADMIN.name(), RoleName.USER.name());
+        registry.requestMatchers(HttpMethod.GET, adminGetEndpoint).hasRole(RoleName.ADMIN.name());
+        registry.requestMatchers(HttpMethod.POST, adminPostEndpoint).hasRole(RoleName.ADMIN.name());
+        registry.requestMatchers(HttpMethod.POST, adminPutEndpoint).hasRole(RoleName.ADMIN.name());
+        registry.requestMatchers(HttpMethod.DELETE,adminDeleteEndpoint).hasRole(RoleName.ADMIN.name());
+
+        registry.anyRequest().authenticated();
     }
 
     //convert từ SCOPE_ qua ROLE_
