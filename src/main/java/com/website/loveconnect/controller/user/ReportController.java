5f31e77df7a0c91792269abed9964c19b0351b72
dinhductai <dinhductai2501@gmail.com>
2025-04-28 08:00:17 +0700

    chore: update class

---
+++ src/main/java/com/website/loveconnect/controller/user/ReportController.java
@@ -0,0 +1,16 @@
+package com.website.loveconnect.controller.user;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequestMapping(value = "/api")
+public class ReportController {
+
+
+}
---
+++ src/main/java/com/website/loveconnect/repository/ReportRepository.java
@@ -0,0 +1,7 @@
+package com.website.loveconnect.repository;
+
+import com.website.loveconnect.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Integer> {
+}
