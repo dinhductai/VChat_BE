5faeaadab56e5e8c8070564677f0d356d4ac53dc
dinhductai <dinhductai2501@gmail.com>
2025-07-31 19:45:06 +0700

    chore: update random post query

--- src/main/java/com/website/loveconnect/repository/query/PostQueries.java
+++ src/main/java/com/website/loveconnect/repository/query/PostQueries.java
@@ -2,42 +2,57 @@ package com.website.loveconnect.repository.query;
 
 public class PostQueries {
     public static final String GET_RANDOM_POST =
-                    "SELECT DISTINCT\n" +
-                    "    u.user_id as userId,\n" +
-                    "    up.full_name as fullName,\n" +
-                    "    up.bio as bio,\n" +
-                    "    u.phone_number as phoneNumber,\n" +
-                    "    prof_pic.photo_url AS profilePicture,\n" +
-                    "    p.post_id as postId,\n" +
-                    "    p.content as content,\n" +
-                    "    p.upload_date as uploadDate,\n" +
-                    "    p.status as status,\n" +
-                    "    p.is_public as isPublic,\n" +
-                    "    GROUP_CONCAT(DISTINCT ph.photo_url SEPARATOR ', ') AS photosUrl,\n" +
-                    "    GROUP_CONCAT(DISTINCT v.video_url SEPARATOR ', ') AS videosUrl\n" +
-                    "FROM posts p\n" +
-                    "JOIN user_posts upo ON p.post_id = upo.post_id\n" +
-                    "JOIN users u ON upo.user_id = u.user_id\n" +
-                    "JOIN user_profiles up ON u.user_id = up.user_id\n" +
-                    "LEFT JOIN photos prof_pic \n" +
-                    "    ON prof_pic.user_id = u.user_id AND prof_pic.is_profile_picture = TRUE\n" +
-                    "LEFT JOIN post_photos pp ON p.post_id = pp.post_id\n" +
-                    "LEFT JOIN photos ph ON pp.photo_id = ph.photo_id\n" +
-                    "LEFT JOIN post_videos pv ON p.post_id = pv.post_id\n" +
-                    "LEFT JOIN video v ON pv.video_id = v.video_id\n" +
-                    "WHERE p.is_public = TRUE\n" +
-                    "GROUP BY \n" +
-                    "    p.post_id,\n" +
-                    "    u.user_id,\n" +
-                    "    up.full_name,\n" +
-                    "    up.bio,\n" +
-                    "    u.phone_number,\n" +
-                    "    prof_pic.photo_url,\n" +
-                    "    p.content,\n" +
-                    "    p.upload_date,\n" +
-                    "    p.status,\n" +
-                    "    p.is_public\n" +
-                    "ORDER BY RAND() ";
+                    "SELECT\n" +
+                            "    u.user_id as userId,\n" +
+                            "    up.full_name as fullName,\n" +
+                            "    up.bio as bio,\n" +
+                            "    u.phone_number as phoneNumber,\n" +
+                            "    prof_pic.photo_url AS profilePicture,\n" +
+                            "    p.post_id as postId,\n" +
+                            "    p.content as content,\n" +
+                            "    p.upload_date as uploadDate,\n" +
+                            "    p.status as status,\n" +
+                            "    p.is_public as isPublic,\n" +
+                            "    GROUP_CONCAT(DISTINCT ph.photo_url SEPARATOR ', ') AS photosUrl,\n" +
+                            "    GROUP_CONCAT(DISTINCT v.video_url SEPARATOR ', ') AS videosUrl\n" +
+                            "FROM posts p\n" +
+                            "JOIN user_posts upo ON p.post_id = upo.post_id\n" +
+                            "JOIN users u ON upo.user_id = u.user_id\n" +
+                            "JOIN user_profiles up ON u.user_id = up.user_id\n" +
+                            "LEFT JOIN (\n" +
+                            "    SELECT\n" +
+                            "        p.user_id,\n" +
+                            "        p.photo_url\n" +
+                            "    FROM photos AS p\n" +
+                            "    INNER JOIN (\n" +
+                            "        SELECT\n" +
+                            "            user_id,\n" +
+                            "            MAX(upload_date) AS latest_date\n" +
+                            "        FROM photos\n" +
+                            "        WHERE is_profile_picture = TRUE\n" +
+                            "        GROUP BY user_id\n" +
+                            "    ) AS latest_pic_dates \n" +
+                            "    ON p.user_id = latest_pic_dates.user_id AND p.upload_date = latest_pic_dates.latest_date\n" +
+                            "    WHERE p.is_profile_picture = TRUE \n" +
+                            "\n" +
+                            ") AS prof_pic ON u.user_id = prof_pic.user_id\n" +
+                            "LEFT JOIN post_photos pp ON p.post_id = pp.post_id\n" +
+                            "LEFT JOIN photos ph ON pp.photo_id = ph.photo_id\n" +
+                            "LEFT JOIN post_videos pv ON p.post_id = pv.post_id\n" +
+                            "LEFT JOIN video v ON pv.video_id = v.video_id\n" +
+                            "WHERE p.is_public = TRUE AND p.is_approved = TRUE AND upo.upload = TRUE\n" +
+                            "GROUP BY\n" +
+                            "    p.post_id,\n" +
+                            "    u.user_id,\n" +
+                            "    up.full_name,\n" +
+                            "    up.bio,\n" +
+                            "    u.phone_number,\n" +
+                            "    prof_pic.photo_url,\n" +
+                            "    p.content,\n" +
+                            "    p.upload_date,\n" +
+                            "    p.status,\n" +
+                            "    p.is_public\n" +
+                            "ORDER BY RAND();";
 
     public static final String GET_ONE_POST_BY_POST_ID =
             "SELECT DISTINCT\n" +
